68e611e8d63bdf3fbca111f02a999946dcf27bf6
Dominique Luna <dluna132@gmail.com>
2018-08-18 02:05:00 -0400

    update

--- fish/config.fish
+++ fish/config.fish
@@ -8,28 +8,29 @@ set fish_greeting ""
 # General stuff
 #
 
+# Linux stuff is mostly for minimal cloud GPU/TPU instance
+
 switch (uname)
 case Linux
-	set -x PATH $HOME/anaconda3/bin $PATH
+	set -x PATH $HOME/julia/bin $PATH
 case Darwin
 	set -x PATH /usr/local/anaconda3/bin $PATH
+	set -x PATH /usr/local/opt/openssl/bin $PATH
+	set -x PATH $HOME/.pulumi/bin $PATH
+	set -x GOPATH (go env GOPATH)
+	set -x PATH $PATH $GOPATH/bin
+	set -x PATH $HOME/julia/usr/bin $PATH
+
+	alias bubu 'brew cleanup; brew prune; brew cask cleanup'
 case '*'
 	echo 'OS not detected!'
 end
-
-set -x GOPATH (go env GOPATH)
-set -x PATH $PATH $GOPATH/bin
-set -x PATH /usr/local/opt/openssl/bin $PATH
-set -x PATH $HOME/julia/usr/bin $PATH
-set -x PATH $HOME/.pulumi/bin $PATH
-
 
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 
 alias gst 'git status'
 alias ga 'git add'
 alias gc 'git commit'
-alias bubu 'brew cleanup; brew prune; brew cask cleanup'
 alias ll 'ls -larth'
 alias md 'mkdir -p'
 
