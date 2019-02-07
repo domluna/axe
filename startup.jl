5f5224dd80a91740a71ae9e0f65eb9da912e99d2
Dominique Luna <dluna132@gmail.com>
2019-02-07 00:44:51 -0500

    ok

--- startup.jl
+++ startup.jl
@@ -2,20 +2,6 @@ atreplinit() do repl
     try
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
-    catch err
+    catch
     end
 end
-
-function _ohmyrepl()
-    try
-        @eval begin
-            using OhMyREPL
-            OhMyREPL.enable_pass!("RainbowBrackets", false)
-            OhMyREPL.enable_pass!("SyntaxHighlighter", false);
-        end
-    catch err
-        @warn "Could not load OhMyREPL.", err
-    end
-end
-
-#= _ohmyrepl() =#
