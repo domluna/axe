b68392097c85d8ff51d89b42ac60297d5376fb03
Dominique Luna <dluna132@gmail.com>
2021-12-26 19:09:22 -0500

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,11 +10,7 @@ end
 
 return require('packer').startup({
   function()
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
-    -- Simple plugins can be specified as strings
-    use '9mm/vim-closer'
 
     use 'sheerun/vim-polyglot'
     use 'JuliaEditorSupport/julia-vim'
