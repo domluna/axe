07f22700629ee1268a8a112c3b18659d11edfde3
Dominique Luna <dluna132@gmail.com>
2022-10-26 21:03:15 -0400

    ok

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -86,7 +86,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
       {update_in_insert = true})
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lspconfig = require('lspconfig')
 
