332baa4e7b15ccffe3e205f8f185aba29c390b29
Dominique Luna <dluna132@gmail.com>
2023-09-05 14:24:34 -0400

    startup

--- startup.jl
+++ startup.jl
@@ -3,14 +3,10 @@ push!(LOAD_PATH, "$(homedir())/.julia/dev")
 if isinteractive()
     import Pkg
     let
-        pkgs = ["Revise", "OhMyREPL", "Term", "IJulia", "JuliaFormatter", "JET", "ReplGPT"]
+        pkgs = ["Revise", "OhMyREPL", "Term", "IJulia", "JuliaFormatter", "JET"]
         for pkg in pkgs
             if Base.find_package(pkg) === nothing
-                if pkg === "REPLVim"
-                    Pkg.add(url = "https://github.com/andreypopp/julia-repl-vim")
-                else
-                    Pkg.add(pkg)
-                end
+                Pkg.add(pkg)
             end
         end
     end
