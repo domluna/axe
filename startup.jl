1fb1fde17a3a1fa206c847c15671df3657323d63
Dominique Luna <dluna132@gmail.com>
2023-10-24 10:55:04 -0400

    j

--- startup.jl
+++ startup.jl
@@ -3,7 +3,7 @@ push!(LOAD_PATH, "$(homedir())/.julia/dev")
 if isinteractive()
     import Pkg
     let
-        pkgs = ["Revise", "OhMyREPL", "JuliaFormatter", "JET"]
+        pkgs = ["Revise", "OhMyREPL", "JuliaFormatter", "JET", "JuliaSyntax"]
         for pkg in pkgs
             if Base.find_package(pkg) === nothing
                 Pkg.add(pkg)
