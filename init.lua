034802f9dd18e42f202b401a519d339343a7bb17
Dominique Luna <dluna132@gmail.com>
2023-12-12 16:19:27 +1000

    up

--- init.lua
+++ init.lua
@@ -21,6 +21,11 @@ require("lazy").setup({
     end,
   },
   {
+    "ellisonleao/gruvbox.nvim",
+    -- priority = 1000,
+    config = true
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     -- priority = 1000, -- make sure to load this before all the other start plugins
