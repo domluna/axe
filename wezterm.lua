935936cb560f04f9a9128c0d91c046c36b710879
Dominique Luna <dluna132@gmail.com>
2024-01-10 13:55:57 -0500

    a

--- wezterm.lua
+++ wezterm.lua
@@ -19,5 +19,18 @@ config.font = wezterm.font 'JuliaMono'
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 -- config.color_scheme = 'Chalk (Gogh)'
 
+config.enable_wayland = true
+  config.mouse_bindings = {
+   -- Ctrl-Shift-C to copy
+    {event={Up={streak=1, button="Left"}},
+     mods="CTRL|SHIFT",
+     action=wezterm.action{CopyTo="Clipboard"}},
+
+    -- Ctrl-Shift-V to paste
+    {event={Down={streak=1, button="Middle"}},
+     mods="CTRL|SHIFT",
+     action=wezterm.action{PasteFrom="Clipboard"}},
+  }
+
 -- and finally, return the configuration to wezterm
 return config
