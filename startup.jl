4834cb1a581a5d2d2f54aeef27397660187067d0
Dominique Luna <dluna132@gmail.com>
2024-07-15 20:30:41 -0400

    done

--- startup.jl
+++ startup.jl
@@ -3,30 +3,49 @@ push!(LOAD_PATH, "$(homedir())/.julia/dev")
 if isinteractive()
     import Pkg
     let
-        pkgs = ["Revise", "OhMyREPL", "JuliaFormatter", "JuliaSyntax", "Debugger", "Chairmarks"]
+        pkgs = [
+            "Revise",
+            "JuliaFormatter",
+            "Debugger",
+            "Chairmarks",
+            "BasicAutoloads",
+            "OhMyREPL"
+            "LanguageServer"
+        ]
         for pkg in pkgs
             if Base.find_package(pkg) === nothing
+                if pkg == "OhMyREPL"
+                    Pkg.add(pkg, version="0.5.24")
+                    Pkg.pin(pkg, version="0.5.24")
+            else
                 Pkg.add(pkg)
+                end
             end
         end
     end
 
-    # import BasicAutoloads
-    # BasicAutoloads.register_autoloads([
-    #     ["@b", "@be"]            => :(using Chairmarks),
-    #     ["@test", "@testset", "@test_broken", "@test_deprecated", "@test_logs",
-    #     "@test_nowarn", "@test_skip", "@test_throws", "@test_warn", "@inferred"] =>
-    #                                 :(using Test),
-    #     ["@about"]               => :(using About; macro about(x) Expr(:call, About.about, x) end),
-    # ])
+    import BasicAutoloads
+    BasicAutoloads.register_autoloads([
+        ["@b", "@be"] => :(using Chairmarks),
+        [
+            "@test",
+            "@testset",
+            "@test_broken",
+            "@test_deprecated",
+            "@test_logs",
+            "@test_nowarn",
+            "@test_skip",
+            "@test_throws",
+            "@test_warn",
+            "@inferred",
+        ] => :(using Test),
+        # ["@about"]               => :(using About; macro about(x) Expr(:call, About.about, x) end),
+    ])
 
     using OhMyREPL
-    # colorscheme!("GruvboxDark")
     # using Debugger
-
     using Revise
 
 
     ENV["JULIA_EDITOR"] = "nvim"
 end
-
