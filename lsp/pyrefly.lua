b497e05fd9b83391dfb627a5aad09486321aadda
Dominique Luna <dluna132@gmail.com>
2025-08-13 01:45:25 -0400

    ok

--- lsp/pyrefly.lua
+++ lsp/pyrefly.lua
@@ -1,6 +1,7 @@
 return {
   cmd = { 'uvx', 'pyrefly', 'lsp' },
   filetypes = { 'python' },
+  root_markers = { 'pyrefly.toml', 'pyproject.toml' },
   settings = {},
   on_exit = function(code, _, _)
     vim.notify('Closing Pyrefly LSP exited with code: ' .. code, vim.log.levels.INFO)
